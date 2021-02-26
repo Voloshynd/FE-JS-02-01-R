36bf575132f0ca1066c89f9c763901305b0a6a30
Dmytro Voloshyn <voloshynd13@gmail.com>
2021-02-26 18:08:24 +0100

    Gender identification by name updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function isMaleName(name) {
   // Zaimplementuj zadanie zgodnie z wytycznymi
-return (name[name.length-1] === 'a' ^ name ==="Bonawentura") ? false : true;
+return (name[name.length-1] === "a" ^ name ==="Bonawentura") ? false : true;
 }
 isMaleName("Ala");
 isMaleName("Jacek");
